22edbb0365c44590686ae8c3717f88870f0a9657
Yuriy Aksenov <Yura290610@yandex.ru>
2018-10-25 17:00:02 +0300

    Изменил структуру проекта

---
+++ ZebraMain/Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZebraData
+{
+  public class Class1
+  {
+  }
+}
---
+++ ZebraMain/Data/Entities/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZebraData.Entities
+{
+  class UserEntity
+  {
+  }
+}
