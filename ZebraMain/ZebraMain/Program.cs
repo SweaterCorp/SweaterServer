9b4a16c4264ccb52c3f82f190d8d99f32307f1cd
Yuriy Aksenov <Yura290610@yandex.ru>
2018-10-27 10:16:04 +0300

    Добавил логику старта в Program

--- ZebraMain/ZebraMain/Program.cs
+++ ZebraMain/ZebraMain/Program.cs
@@ -12,13 +12,46 @@ namespace ZebraMain
 {
   public class Program
   {
+    public static IConfiguration Configuration { get; private set; }
+    public static string Scheme { get; } = "http";
+    public static string IpAddress { get; } = "localhost";
+    public static string Port { get; private set; }
+    public static string Url => Scheme + "://" + IpAddress + ":" + Port;
+
     public static void Main(string[] args)
     {
       CreateWebHostBuilder(args).Build().Run();
     }
 
-    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-        WebHost.CreateDefaultBuilder(args)
-            .UseStartup<Startup>();
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+    {
+      var builder = new ConfigurationBuilder().SetBasePath(AppContext.BaseDirectory);
+
+      builder.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "appsettings.json"));
+      builder.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "..", "routingsettings.json"));
+      builder.AddJsonFile(Path.Combine(AppContext.BaseDirectory, "..", "hiddensettings.json"));
+
+      Configuration = builder.Build();
+      Port = Configuration["WebHost:Port"];
+
+      var commandLine = Command.WithName("host").HasOption("-port", "-p");
+
+      if (commandLine.TryParse(args, out var command) && command is HostCommand hostCommand &&
+          !hostCommand.Port.IsNullOrEmpty()) Port = hostCommand.Port;
+      return WebHost.CreateDefaultBuilder(args).UseConfiguration(Configuration).UseUrls(Url)
+        .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Trace)).UseStartup<Startup>().UseSerilog(
+          (ctx, cfg) =>
+          {
+            var serverName = Configuration.GetValue<string>("ServerName");
+            var path = Path.Combine(AppContext.BaseDirectory, ".." + Path.DirectorySeparatorChar, "Logs",
+              serverName + "{Date}" + ".log");
+
+            cfg.ReadFrom.Configuration(ctx.Configuration).MinimumLevel.Debug().MinimumLevel
+              .Override("Microsoft", LogEventLevel.Information).WriteTo
+              .RollingFile(path,
+                outputTemplate:
+                "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} {Application} {Level:u3}] {SourceContext}: {Message:lj}{NewLine}{Exception}"); //,fileSizeLimitBytes:500*1024*1024);
+          }).Build();
+    }
   }
 }
