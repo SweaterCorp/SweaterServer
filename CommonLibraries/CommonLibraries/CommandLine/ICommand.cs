6c0ae53e8b3a20edc434448dd33a48c0b2130565
Yuriy Aksenov <Yura290610@yandex.ru>
2018-10-27 10:14:57 +0300

    Добавил CommandLine

---
+++ CommonLibraries/CommonLibraries/CommandLine/ICommand.cs
@@ -0,0 +1,7 @@
+﻿namespace CommonLibraries.CommandLine
+{
+  public interface ICommand
+  {
+    void Run();
+  }
+}
---
+++ CommonLibraries/CommonLibraries/CommandLine/OrCommandLinePattern.cs
@@ -0,0 +1,19 @@
+﻿namespace CommonLibraries.CommandLine
+{
+  public class OrCommandLinePattern : CommandLinePattern
+  {
+    private readonly CommandLinePattern _left;
+    private readonly CommandLinePattern _right;
+
+    public OrCommandLinePattern(CommandLinePattern left, CommandLinePattern right)
+    {
+      _left = left;
+      _right = right;
+    }
+
+    public override bool TryParse(string[] args, out Command result)
+    {
+      return _left.TryParse(args, out result) || _right.TryParse(args, out result);
+    }
+  }
+}
