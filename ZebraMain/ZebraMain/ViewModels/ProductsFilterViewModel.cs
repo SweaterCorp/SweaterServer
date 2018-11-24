504f4da8f4654b2972f59941714ba6f2d0e8bba2
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-24 09:10:51 +0300

    MVP. Добавил фильтры - объединение параметров

---
+++ ZebraMain/ZebraMain/ViewModels/ProductsFilterViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ZebraMain.ViewModels
+{
+  public class ProductsFilterViewModel
+  {
+    public decimal MinimalPrice { get; set; }
+    public decimal MaximalPrice { get; set; }
+    public List<int> BrandsIds { get; set; }
+    public List<int> ColorsIds { get; set; }
+    public List<int> SizesIds { get; set; }
+    public PageParams PageParams { get; set; }
+  }
+}
