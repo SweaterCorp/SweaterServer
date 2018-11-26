3d84a193eb9185c2ec6fbdfcde5eb3c047815736
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-26 05:48:38 +0300

    Унифицировал UserViwModel

--- ZebraMain/ZebraMain/ViewModels/CreateUserViewModel.cs
+++ ZebraMain/ZebraMain/ViewModels/CreateUserViewModel.cs
@@ -3,16 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ZebraMain.ViewModels
 {
-  public class CreateUserViewModel
+  public class UserViewModel
   {
-    [Required]
-    public int UserId { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public int HumanColorTypeId { get; set; }
-    public int ShapeTypeId { get; set; }
-    public int SexTypeId { get; set; }
-    public DateTime BirthDate { get; set; }
-    public int CityId { get; set; }
+    public int? HumanColorTypeId { get; set; }
+    public int? ShapeTypeId { get; set; }
+    public int? SexTypeId { get; set; }
   }
 }
