285a6eb8cbe3997d7b529a157c235217bd46b537
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-27 03:41:41 +0300

    Добавил класс для более легкого перееноса и преобразования web to pc path

---
+++ ZebraMain/ZebraMain/Infrastructure/ServerUrl.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+
+namespace ZebraMain.Infrastructure
+{
+  public class ServerUrl
+  {
+    private string[] Fragments { get; }
+
+    public ServerUrl(string url)
+    {
+      Fragments = url.Contains('/') ? url.Split('/') : url.Split('\\');
+    }
+
+    public ServerUrl(string[] fragments)
+    {
+      Fragments = fragments;
+    }
+
+    public string ToPcUrl()
+    {
+      return Path.Combine(Fragments);
+    }
+
+    public string ToWebUrl()
+    {
+      return string.Join('/', Fragments);
+    }
+
+    public static bool IsNullOrEmpty(ServerUrl serverUrl)
+    {
+      return serverUrl == null || serverUrl.Fragments == null || serverUrl.Fragments.Length < 0;
+    }
+  }
+}
