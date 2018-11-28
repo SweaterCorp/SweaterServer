23c8593b534d9034eb132c3ca9d6322d64eb35c2
Yuriy Aksenov <Yura290610@yandex.ru>
2018-11-28 23:22:58 +0300

    Добавил класс для отображения типов в локализациях

---
+++ CommonLibraries/CommonLibraries/Localization/TranslationName.cs
@@ -0,0 +1,25 @@
+﻿namespace CommonLibraries.Localization
+{
+  public class TranslationName
+  {
+    public LaguageType LaguageType { get; set; }
+    public string Name { get; set; }
+
+    public TranslationName()
+    {
+    }
+
+    public TranslationName(LaguageType laguage, string name)
+    {
+      LaguageType = laguage;
+      Name = name;
+    }
+  }
+
+  public enum LaguageType
+  {
+    Default = 0,
+    Russian = 1,
+    English = 2
+  }
+}
