209c0a93fc8728da58557e2460d69b0662c91689
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 12:48:17 +0700

    [Refactor] Move RoleType to Archive

---
+++ CommonLibraries/CommonLibraries/Old/NotRoleType.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.Infrastructures;
+
+namespace CommonLibraries.Old
+{
+  public class NotRoleType : Enumeration
+  {
+    public static NotRoleType Guest { get; } = new NotRoleType(0, "Guest");
+    public static NotRoleType User { get; } = new NotRoleType(1, "User");
+    public static NotRoleType Moderator { get; } = new NotRoleType(2, "Moderator");
+    public static NotRoleType Administrator { get; } = new NotRoleType(3, "Administrator");
+
+    private NotRoleType(int id, string name) : base(id, name)
+    {
+    }
+
+    public static IEnumerable<NotRoleType> List()
+    {
+      return new List<NotRoleType> {Guest, User, Moderator, Administrator};
+    }
+
+    public static NotRoleType FromString(string name)
+    {
+      return FromString(name, List());
+    }
+
+    public static NotRoleType FromValue(int id)
+    {
+      return FromValue(id, List());
+    }
+  }
+}
