10a7acf06f0e060ff81c77b89dc6a07e6b44bcfe
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 22:14:30 +0700

    [Add] Resources handler to read resources

---
+++ ZebraMain/ZebraMain/Resources/ResourceHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ZebraMain.Resources
+{
+  public class ResourceHandler
+  {
+    public string ReadResourceFile(string path)
+    {
+      using (var sr = new StreamReader(path, Encoding.UTF8))
+      {
+        var str = sr.ReadToEnd();
+        return str;
+      }
+    }
+
+    public T ReadeResourceFile<T>(string path)
+    {
+      using (var sr = new StreamReader(path, Encoding.UTF8))
+      {
+        var str = sr.ReadToEnd();
+        var result = JsonConvert.DeserializeObject<T>(str);
+        return result;
+      }
+    }
+  }
+}
