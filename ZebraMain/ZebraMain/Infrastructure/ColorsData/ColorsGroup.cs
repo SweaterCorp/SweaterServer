471281ec1af19f27d2bc8bc81a43cdb2820c8347
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 22:11:35 +0700

    [Add] ColorsGroup for godd/bad base colors

---
+++ ZebraMain/ZebraMain/Infrastructure/ColorsData/ColorsGroup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.Infrastructures;
+
+namespace ZebraMain.Infrastructure.ColorsData
+{
+  public class ColorsGroup
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<BaseColor> BaseColors { get; set; }
+    public List<ServerColor> GoodColors { get; set; }
+    public List<ServerColor> BadColors { get; set; }
+  }
+}
