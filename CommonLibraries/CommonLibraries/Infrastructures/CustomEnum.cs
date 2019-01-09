d851ba0d905eb60883d15f4a4f9c7dd44c751dae
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 22:14:47 +0700

    [Add] Custom Enum

---
+++ CommonLibraries/CommonLibraries/Infrastructures/CustomEnum.cs
@@ -0,0 +1,18 @@
+﻿namespace CommonLibraries.Infrastructures
+{
+  public class CustomEnum
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    protected CustomEnum()
+    {
+    }
+
+    public CustomEnum(int id, string name)
+    {
+      Id = id;
+      Name = name;
+    }
+  }
+}
