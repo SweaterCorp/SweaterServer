3cc3ee8a664cff4e07bc0eb68abb6cacf582010d
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 22:14:08 +0700

    [Add] ContentPath to get to resources folder

---
+++ ZebraMain/ZebraMain/Resources/ContentPath.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ZebraMain.Resources
+{
+  public class Content
+  {
+    private string ContentPath { get; }
+    public string Reources => Path.Combine(ContentPath, "Resources");
+
+    public Content(string basePath)
+    {
+      ContentPath = basePath;
+    }
+
+    public Content(IHostingEnvironment env)
+    {
+      ContentPath = env.ContentRootPath;
+    }
+
+    public string GetReourcePath(string resourceName)
+    {
+      return Path.Combine(ContentPath, "Resources", resourceName);
+    }
+  }
+}
