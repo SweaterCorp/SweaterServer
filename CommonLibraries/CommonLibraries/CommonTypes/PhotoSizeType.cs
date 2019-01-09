0fc46c877761e1be679379fb42670eeb4126a81e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 22:11:01 +0700

    [Add] PhotoSizeType

---
+++ CommonLibraries/CommonLibraries/CommonTypes/PhotoSizeType.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.Infrastructures;
+
+namespace CommonLibraries.CommonTypes
+{
+  public class PhotoSizeType : CustomEnum
+  {
+    public static PhotoSizeType None { get; } = new PhotoSizeType(0, "None");
+    public static PhotoSizeType Original { get; } = new PhotoSizeType(1, "Original");
+    public static PhotoSizeType Small { get; } = new PhotoSizeType(2, "Small");
+
+    public PhotoSizeType(int id, string name) : base(id, name)
+    {
+    }
+
+    public static List<PhotoSizeType> AsList()
+    {
+      return new List<PhotoSizeType> {None, Original, Small};
+    }
+
+    public static explicit operator PhotoSizeType(int id)
+    {
+      return AsList().Find(x => x.Id == id);
+    }
+  }
+}
