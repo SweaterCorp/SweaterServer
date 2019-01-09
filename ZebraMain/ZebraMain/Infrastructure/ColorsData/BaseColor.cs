edb68a7fb6a8f2756625ad2020c7157adc8650cb
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 22:11:17 +0700

    [Add] BaseColor

---
+++ ZebraMain/ZebraMain/Infrastructure/ColorsData/BaseColor.cs
@@ -0,0 +1,11 @@
+﻿using CommonLibraries.CommonTypes;
+using CommonLibraries.Infrastructures;
+
+namespace ZebraMain.Infrastructure.ColorsData
+{
+  public class BaseColor
+  {
+    public ColorType ColorType { get; set; }
+    public ServerColor Color { get; set; }
+  }
+}
