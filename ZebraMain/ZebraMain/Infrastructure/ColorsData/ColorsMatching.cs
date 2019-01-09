90397866c01c5a55f0af6802979e31284ca0c5f8
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-09 22:12:19 +0700

    [Add] ColorsMatching 4 tons

---
+++ ZebraMain/ZebraMain/Infrastructure/ColorsData/ColorsMatching.cs
@@ -0,0 +1,10 @@
+﻿namespace ZebraMain.Infrastructure.ColorsData
+{
+  public class ColorsMatching
+  {
+    public ColorsGroups Autumn { get; set; }
+    public ColorsGroups Spring { get; set; }
+    public ColorsGroups Summer { get; set; }
+    public ColorsGroups Winter { get; set; }
+  }
+}
