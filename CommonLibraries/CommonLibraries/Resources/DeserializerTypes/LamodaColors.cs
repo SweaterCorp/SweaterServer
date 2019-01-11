004b74406382972adeea0eed67b2e01b7f6eff3b
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 14:48:40 +0700

    [Add] LamovaColors

---
+++ CommonLibraries/CommonLibraries/Resources/DeserializerTypes/LamodaColors.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CommonLibraries.Resources.DeserializerTypes
+{
+  public class DeserializerLamodaColors
+  {
+    public List<DeserializerColorCollection> Colors { get; set; }
+  }
+
+  public class DeserializerColorCollection
+  {
+    public DeserializerLamodaColorType LamodaColorType { get; set; }
+    public List<string> HexColors { get; set; }
+  }
+
+  public class DeserializerLamodaColorType
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
