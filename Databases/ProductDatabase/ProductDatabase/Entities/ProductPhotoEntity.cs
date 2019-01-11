047d04cca2125d1f93095bafc77d33554e8b46a2
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 15:51:12 +0700

    [Add] ProductPhotoEntity

---
+++ Databases/ProductDatabase/ProductDatabase/Entities/ProductPhotoEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductDatabase.Entities
+{
+  [Table("ProductPhoto")]
+  public class ProductPhotoEntity
+  {
+    [Key]
+    public int ProductPhotoId { get; set; }
+
+    public int ProductId { get; set; }
+    public string PhotoUrl { get; set; }
+  }
+}
