5ca659998c9ac4e062a0af59752bc39e37ec9de6
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 15:51:22 +0700

    [Add] ProductSizedEntity

---
+++ Databases/ProductDatabase/ProductDatabase/Entities/ProductSizeTypeEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductDatabase.Entities
+{
+  [Table("ProductSizeType")]
+  public class ProductSizeTypeEntity
+  {
+    [Key]
+    public int ProductId { get; set; }
+    public int SizeTypeId { get; set; }
+    public bool IsAvailable { get; set; }
+    public DateTime UpdatedDate { get; set; }
+  }
+}
