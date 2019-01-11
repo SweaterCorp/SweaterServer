fd9678eef0c63ca9c1c858bd5c33be4fdb5302c6
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 14:51:05 +0700

    [Refactor] Comment validation

---
+++ ZebraMain/ZebraMain/Infrastructure/TypeValidation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonLibraries.Infrastructures;
+
+namespace ZebraMain.Infrastructure
+{
+  //public class TypeAttribute<T> : ValidationAttribute
+  //{
+  //  //массив для хранения допустимых имен
+  //  string[] _names;
+
+  //  public TypeAttribute(Type)
+  //  {
+  //    _names = names;
+  //  }
+  //  public override bool IsValid(object value)
+  //  {
+  //    if (_names.Contains(value.ToString()))
+  //      return true;
+
+  //    return false;
+  //  }
+  //}
+}
