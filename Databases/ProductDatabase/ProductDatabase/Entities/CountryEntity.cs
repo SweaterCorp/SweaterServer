ee1cf9d5e1fff62d7db2d861b9dbd69f77aa605e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 15:50:55 +0700

    [Add] CountryEntity

---
+++ Databases/ProductDatabase/ProductDatabase/Entities/CountryEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductDatabase.Entities
+{
+  [Table("Country")]
+  public class CountryEntity
+  {
+    [Key]
+    public int CountryId { get; set; }
+
+    public string FlagUrl { get; set; }
+    public string EnglishName { get; set; }
+    public string RussianName { get; set; }
+  }
+}
