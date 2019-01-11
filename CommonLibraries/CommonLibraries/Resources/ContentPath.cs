4af73d12b931a9bad26c24c430caf17c0f5af3f3
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 14:48:34 +0700

    [Add] ContentPath

---
+++ CommonLibraries/CommonLibraries/Resources/ContentPath.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CommonLibraries.Resources
+{
+  public class Content
+  {
+    private string ContentPath { get; }
+    public string Reources => Path.Combine(ContentPath, "Resources");
+
+    public string ColorsMatching => Path.Combine(ContentPath, "ColorsMatching.json");
+    public string LamodaColors => Path.Combine(ContentPath, "LamodaColors.json");
+
+    public Content(string basePath)
+    {
+      ContentPath = basePath;
+    }
+
+    public Content(IHostingEnvironment env)
+    {
+      ContentPath = env.ContentRootPath;
+    }
+
+    public string GetReourcePath(string resourceName)
+    {
+      return Path.Combine(ContentPath, "Resources", resourceName);
+    }
+  }
+}
