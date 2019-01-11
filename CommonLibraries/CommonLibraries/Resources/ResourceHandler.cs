19c9a214f2c6fb27be9e04282b0ca3dd7b39265f
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 14:49:05 +0700

    [Add] ResourceHandler

---
+++ CommonLibraries/CommonLibraries/Resources/ResourceHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CommonLibraries.Resources
+{
+  public class ResourceHandler
+  {
+    public string ReadResourceFile(string path)
+    {
+      using (var sr = new StreamReader(path, Encoding.UTF8))
+      {
+        var str = sr.ReadToEnd();
+        return str;
+      }
+    }
+
+    public T ReadeResourceFile<T>(string path)
+    {
+      using (var sr = new StreamReader(path, Encoding.UTF8))
+      {
+        var str = sr.ReadToEnd();
+        var result = JsonConvert.DeserializeObject<T>(str);
+        return result;
+      }
+    }
+  }
+}
