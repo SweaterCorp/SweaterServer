30ae5b0fb3182c27de998759d2da6811bfb48f3f
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 14:50:26 +0700

    [Add] LamodaColors

---
+++ ZebraMain/ZebraMain/Infrastructure/ColorsData/LamodaColors.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.CommonTypes;
+using CommonLibraries.Infrastructures;
+
+namespace ZebraMain.Infrastructure.ColorsData
+{
+  public class LamodaColors
+  {
+    public List<ColorCollection> Colors { get; set; }
+  }
+
+  public class ColorCollection
+  {
+    public LamodaColorType LamodaColorType { get; set; }
+    public List<ServerColor> ServerColors { get; set; }
+  }
+}
