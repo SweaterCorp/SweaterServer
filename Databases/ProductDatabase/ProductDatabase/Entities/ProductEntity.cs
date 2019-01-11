a9b076453da414545cd2f0602a87fb1dff53c34e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 15:51:02 +0700

    [Add] ProductEntity

---
+++ Databases/ProductDatabase/ProductDatabase/Entities/ProductEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductDatabase.Entities
+{
+  [Table("Product")]
+  public class ProductEntity
+  {
+    [Key]
+    public int ProductId { get; set; }
+
+    public int BrandId { get; set; }
+    public int ColorId { get; set; }
+    public int ClicksCount { get; set; }
+    public string VendorCode { get; set; }
+    public bool IsAvailable { get; set; }
+    public int CategoryId { get; set; }
+    public int PrintTypeId { get; set; }
+    public decimal Price { get; set; }
+    public string Description { get; set; }
+    public int MadeInCountryId { get; set; }
+    public string Link { get; set; }
+    public string PreviewPhotoUrl { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public bool IsDeleted { get; set; }
+  }
+}
