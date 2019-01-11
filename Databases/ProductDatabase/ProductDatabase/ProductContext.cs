6c099861c1335b89c1e19c93f22a5ae5e86fbe18
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 15:51:42 +0700

    [Add] ProductContext

---
+++ Databases/ProductDatabase/ProductDatabase/ProductContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductDatabase.Entities;
+
+namespace ProductDatabase
+{
+  public class ProductContext : DbContext
+  {
+    public virtual DbSet<BrandEntity> BrandEntities { get; set; }
+    public virtual DbSet<CategoryEntity> CategoryEntities { get; set; }
+    public virtual DbSet<ProductEntity> ProductEntities { get; set; }
+    public virtual DbSet<ProductPhotoEntity> ProductPhotoEntities { get; set; }
+    public virtual DbSet<ProductSizeTypeEntity> ProductSizeTypeEntities { get; set; }
+    public virtual DbSet<SizeTypeEntity> SizeTypeEntities { get; set; }
+    public virtual DbSet<CountryEntity> CountryEntities { get; set; }
+
+    public ProductContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      //modelBuilder.Entity<UserEntity>().Property(a => a.HumanColorType).HasColumnType("int");
+      //modelBuilder.Entity<UserEntity>().Property(a => a.HumanColorType).Ignore(x => x.HumanColorType.Name);
+
+      //modelBuilder.Entity<UserEntity>().Property(a => a.SexType)..HasColumnType("int");
+      //modelBuilder.Entity<UserEntity>().Ignore(x => x.SexType.Name);
+
+      //modelBuilder.Entity<UserEntity>().Property(a => a.ShapeType).HasColumnType("int");
+      //modelBuilder.Entity<UserEntity>().Ignore(x => x.ShapeType.Name);
+
+      //modelBuilder.Entity<UserPhotoEntity>().HasKey(x => new { x. });
+      //modelBuilder.Entity<ProductSizeTypeEntity>().HasKey(x => new { x.ProductId, x.SizeTypeId });
+      //modelBuilder.Entity<ProductColorTypeEntity>().HasKey(x => new { x.ProductId, x.ColorTypeId });
+    }
+  }
+}
