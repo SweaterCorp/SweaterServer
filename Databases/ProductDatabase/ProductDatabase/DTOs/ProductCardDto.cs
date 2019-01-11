a277c68d85e8468801db767892c01ebefec2b14c
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 17:05:06 +0700

    [Add] Product Dto

---
+++ Databases/ProductDatabase/ProductDatabase/DTOs/ProductCardDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ProductDatabase.Entities;
+
+namespace ProductDatabase.DTOs
+{
+  public class ProductCardDto
+  {
+    public BrandEntity Brand { get; set; }
+    public ProductEntity Product { get; set; }
+    public List<SizeTypeEntity> Sizes { get; set; }
+    public int Color { get; set; }
+  }
+}
