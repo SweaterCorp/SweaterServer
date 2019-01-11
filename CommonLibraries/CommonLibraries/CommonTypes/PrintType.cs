79499a857a2137e764a80335d089d0f0e82ccc7a
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 13:13:02 +0700

    [Add] PrintTYpe

--- CommonLibraries/CommonLibraries/CommonTypes/PrintType.cs
+++ CommonLibraries/CommonLibraries/CommonTypes/PrintType.cs
@@ -3,65 +3,50 @@ using CommonLibraries.Infrastructures;
 
 namespace CommonLibraries.CommonTypes
 {
-  public class CategoryType : CustomEnum
+  public class PrintType : CustomEnum
   {
-    public static CategoryType None { get; } = new CategoryType(0, "None");
-    public static CategoryType BlousesShirts { get; } = new CategoryType(1, "BlousesShirts");
-    public static CategoryType Trousers { get; } = new CategoryType(2, "Trousers");
-    public static CategoryType Outerwear { get; } = new CategoryType(3, "Outerwear");
-    public static CategoryType JumpersSweatersCardigans { get; } = new CategoryType(4, "JumpersSweatersCardigans");
-    public static CategoryType Jeans { get; } = new CategoryType(5, "Jeans");
-    public static CategoryType JacketsSuits { get; } = new CategoryType(6, "JacketsSuits");
-    public static CategoryType Dresses { get; } = new CategoryType(7, "Dresses");
-    public static CategoryType Tracksuits { get; } = new CategoryType(8, "Tracksuits");
-    public static CategoryType HoodiesSweatshirts { get; } = new CategoryType(9, "HoodiesSweatshirts");
-    public static CategoryType Tops { get; } = new CategoryType(10, "Tops");
-    public static CategoryType TshirtsPolo { get; } = new CategoryType(11, "TShirtsPolo");
-    public static CategoryType Shorts { get; } = new CategoryType(12, "Shorts");
-    public static CategoryType Skirts { get; } = new CategoryType(13, "Skirts");
+    public string RussianName { get; set; }
 
-    private CategoryType(int id, string name) : base(id, name)
+    public static PrintType None { get; } = new PrintType(0, "None", "None");
+    public static PrintType Geometry { get; } = new PrintType(1, "Geometry;", "Геометрия");
+    public static PrintType PolkaDotted { get; } = new PrintType(2, "Polka-dotted", "Горох");
+    public static PrintType Other { get; } = new PrintType(3, "Other", "Другое");
+    public static PrintType Animals { get; } = new PrintType(4, "Animals", "Животные");
+    public static PrintType Camouflage { get; } = new PrintType(5, "Camouflage", "Камуфляж");
+    public static PrintType Checked { get; } = new PrintType(6, "Checked", "Клетка");
+    public static PrintType Spotted { get; } = new PrintType(7, "Spotted", "Леопардовый");
+    public static PrintType Plain { get; } = new PrintType(8, "Plain", "Однотонный");
+    public static PrintType Striped { get; } = new PrintType(9, "Striped", "Полоска");
+    public static PrintType Printed { get; } = new PrintType(10, "Printed", "Рисунки и надписи");
+    public static PrintType Floral { get; } = new PrintType(11, "Floral", "Цветочный");
+
+    private PrintType(int id, string name, string russianName = "") : base(id, name)
     {
+      RussianName = russianName;
     }
 
-    public static List<CategoryType> AsList()
+    public static List<PrintType> AsList()
     {
-      return new List<CategoryType>
+      return new List<PrintType>
       {
         None,
-        BlousesShirts,
-        Trousers,
-        Outerwear,
-        JumpersSweatersCardigans,
-        Jeans,
-        JacketsSuits,
-        Dresses,
-        Tracksuits,
-        HoodiesSweatshirts,
-        Tops,
-        TshirtsPolo,
-        Shorts,
-        Skirts
+        Geometry,
+        PolkaDotted,
+        Other,
+        Animals,
+        Camouflage,
+        Checked,
+        Spotted,
+        Plain,
+        Striped,
+        Printed,
+        Floral
       };
     }
 
-    public static explicit operator CategoryType(int id)
+    public static explicit operator PrintType(int id)
     {
       return AsList().Find(x => x.Id == id);
     }
   }
-
-  //("blouses_shirts","https://www.lamoda.ru/c/399/clothes-bluzy-rubashki/"),
-  //("trousers","https://www.lamoda.ru/c/401/clothes-bryuki-shorty-kombinezony/"),
-  //("outerwear","https://www.lamoda.ru/c/357/clothes-verkhnyaya-odezhda/"),
-  //("jumpers_sweaters_cardigans","https://www.lamoda.ru/c/371/clothes-trikotazh/"),
-  //("jeans","https://www.lamoda.ru/c/397/clothes-d-insy/"),
-  //("jackets_suits","https://www.lamoda.ru/c/367/clothes-pidzhaki-zhaketi/"),
-  //("dresses","https://www.lamoda.ru/c/369/clothes-platiya/"),
-  //("tracksuits","https://www.lamoda.ru/c/415/clothes-kostyumy/"),
-  //("hoodies_sweatshirts","https://www.lamoda.ru/c/2474/clothes-tolstovki-olimpiyki/"),
-  //("tops","https://www.lamoda.ru/c/2627/clothes-topy/"),
-  //("t_shirts_polo","https://www.lamoda.ru/c/2478/clothes-futbolki/"),
-  //("shorts","https://www.lamoda.ru/c/2485/clothes-shorty/"),
-  //("skirts","https://www.lamoda.ru/c/423/clothes-yubki/")
 }
