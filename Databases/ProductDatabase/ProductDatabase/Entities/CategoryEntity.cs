3b581ed6ab79262ecbd4e1fd9fccb9a73f4d7fe9
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-11 15:50:42 +0700

    [Add] CategoryEntity

---
+++ Databases/ProductDatabase/ProductDatabase/Entities/CategoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductDatabase.Entities
+{
+  [Table("Category")]
+  public class CategoryEntity
+  {
+    [Key]
+    public int CategoryId { get; set; }
+    public int CategoryPhoroUrl { get; set; }
+    public int ClicksCount { get; set; }
+    public string CategoryPhotoUrl { get; set; }
+  }
+}
