8e214ea7d6edeb27b3a8fec3a4ba35bf64f39fe9
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-12 14:45:19 +0700

    [Add] ProductDto

---
+++ Databases/ProductDatabase/ProductDatabase/DTOs/AddProductDto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace ProductDatabase.DTOs
+{
+  public class AddProductDto
+  {
+    public string VendorCode { get; set; }
+    public string BrandName { get; set; }
+    public int CategoryTypeId { get; set; }
+    public int PrintTypeId { get; set; }
+    public int ColorId { get; set; }
+    public decimal Price { get; set; }
+    public string Country { get; set; }
+    public string Link { get; set; }
+    public List<SizeDto> Sizes { get; set; }
+    public List<string> Photos { get; set; }
+  }
+
+  public class SizeDto
+  {
+    public bool IsAvailable { get; set; }
+    public string RussianSize { get; set; }
+    public string OtherSize { get; set; }
+    public string CountryCode { get; set; }
+  }
+}
