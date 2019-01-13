0e03468ccc44c577240f14d4c4891b0f5cf319fd
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-13 12:30:22 +0700

    [Remove] Extra attr "Key"

--- Databases/ProductDatabase/ProductDatabase/Entities/ProductSizeTypeEntity.cs
+++ Databases/ProductDatabase/ProductDatabase/Entities/ProductSizeTypeEntity.cs
@@ -7,7 +7,6 @@ namespace ProductDatabase.Entities
   [Table("ProductSizeType")]
   public class ProductSizeTypeEntity
   {
-    [Key]
     public int ProductId { get; set; }
     public int SizeTypeId { get; set; }
     public bool IsAvailable { get; set; }
