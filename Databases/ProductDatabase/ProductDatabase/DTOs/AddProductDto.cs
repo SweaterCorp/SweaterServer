dfb941761ff4e292b3a9e40e57258163cc8ada73
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-13 12:29:42 +0700

    [Add] IEquality comparer realization

--- Databases/ProductDatabase/ProductDatabase/DTOs/AddProductDto.cs
+++ Databases/ProductDatabase/ProductDatabase/DTOs/AddProductDto.cs
@@ -8,6 +8,7 @@ namespace ProductDatabase.DTOs
     public string BrandName { get; set; }
     public int CategoryTypeId { get; set; }
     public int PrintTypeId { get; set; }
+    public int ExtraPrintTypeId { get; set; }
     public int ColorId { get; set; }
     public decimal Price { get; set; }
     public string Country { get; set; }
@@ -16,11 +17,22 @@ namespace ProductDatabase.DTOs
     public List<string> Photos { get; set; }
   }
 
-  public class SizeDto
+  public class SizeDto : IEqualityComparer<SizeDto>
   {
     public bool IsAvailable { get; set; }
     public string RussianSize { get; set; }
-    public string OtherSize { get; set; }
+    public string OtherCountry { get; set; }
     public string CountryCode { get; set; }
+
+    public bool Equals(SizeDto x, SizeDto y)
+    {
+      return x.RussianSize == y.RussianSize && x.OtherCountry == y.OtherCountry &&
+        x.CountryCode == y.CountryCode;
+    }
+
+    public int GetHashCode(SizeDto obj)
+    {
+      return obj.RussianSize.GetHashCode() ^ obj.OtherCountry.GetHashCode() ^ obj.CountryCode.GetHashCode();
+    }
   }
 }
