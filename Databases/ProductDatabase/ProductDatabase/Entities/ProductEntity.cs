3d045b61b5eca54edb6ea36be0e8863a4baa0d08
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-13 12:29:55 +0700

    [Add] New field to product entity

--- Databases/ProductDatabase/ProductDatabase/Entities/ProductEntity.cs
+++ Databases/ProductDatabase/ProductDatabase/Entities/ProductEntity.cs
@@ -11,12 +11,14 @@ namespace ProductDatabase.Entities
     public int ProductId { get; set; }
 
     public int BrandId { get; set; }
-    public int ColorId { get; set; }
+    public int ShopColorId { get; set; }
+    public int ShopTypeId { get; set; }
     public int ClicksCount { get; set; }
     public string VendorCode { get; set; }
     public bool IsAvailable { get; set; }
     public int CategoryId { get; set; }
     public int PrintTypeId { get; set; }
+    public int ExtraPrintTypeId { get; set; }
     public decimal Price { get; set; }
     public string Description { get; set; }
     public int MadeInCountryId { get; set; }
