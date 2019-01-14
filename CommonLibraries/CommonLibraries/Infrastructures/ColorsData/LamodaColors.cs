d117bdc71b9f656ceecb8cc6bc5e6ef8a6db1d6e
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-14 17:29:31 +0700

    [Add] LamodaColors

---
+++ CommonLibraries/CommonLibraries/Infrastructures/ColorsData/LamodaColors.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.CommonTypes;
+
+namespace CommonLibraries.Infrastructures.ColorsData
+{
+  public class LamodaColors
+  {
+    public List<ColorCollection> Colors { get; set; }
+  }
+
+  public class ColorCollection
+  {
+    public LamodaColorType LamodaColorType { get; set; }
+    public List<ServerColor> ServerColors { get; set; }
+  }
+}
