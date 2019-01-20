07324b29b45e236d77a15a23ea04470d0354d005
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-20 14:13:06 +0700

    [Add] ListExtension

---
+++ CommonLibraries/CommonLibraries/Extensions/ListExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CommonLibraries.Extensions
+{
+  public static class ListExtension
+  {
+    public static T GetValueOrDefault<T>(this List<T> list, int index)
+    {
+      return GetValueOrDefault(list, index, default(T));
+    }
+
+    public static T GetValueOrDefault<T>(this List<T> list, int index, T @default)
+    {
+      return list.Count >= index + 1 ? list[index] : @default;
+    }
+  }
+}
