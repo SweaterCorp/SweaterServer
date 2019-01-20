1b958f9c744b3e214abd7346209e15da682f6684
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-20 14:12:56 +0700

    [Add] DoubleExtension

---
+++ CommonLibraries/CommonLibraries/Extensions/DoubleExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommonLibraries.Extensions
+{
+  public static class DoubleExtension
+  {
+    public static bool EqualsStrict(this double left, double right, double tolerance = 0.000001)
+    {
+      return Math.Abs(left - right) < tolerance;
+    }
+  }
+}
