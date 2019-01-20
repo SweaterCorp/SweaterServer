8a178f3aa0e14dbf834d1a82956a1a8180bde488
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-20 14:12:46 +0700

    [Add] PersonalColorType

---
+++ CommonLibraries/CommonLibraries/CommonTypes/PersonalColorType.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.Infrastructures;
+
+namespace CommonLibraries.CommonTypes
+{
+  public class PersonalColorType : CustomEnum
+  {
+    public static PersonalColorType None { get; } = new PersonalColorType(0, "None");
+    public static PersonalColorType Winter { get; } = new PersonalColorType(1, "Winter");
+    public static PersonalColorType Spring { get; } = new PersonalColorType(2, "Spring");
+    public static PersonalColorType Summer { get; } = new PersonalColorType(3, "Summer");
+    public static PersonalColorType Autumn { get; } = new PersonalColorType(4, "Autumn");
+
+    private PersonalColorType(int id, string name) : base(id, name)
+    {
+    }
+
+    public static List<PersonalColorType> AsList()
+    {
+      return new List<PersonalColorType> {None, Winter, Spring, Summer, Autumn};
+    }
+
+    public static explicit operator PersonalColorType(int id)
+    {
+      return AsList().Find(x => x.Id == id);
+    }
+
+    public static explicit operator PersonalColorType(string name)
+    {
+      return AsList().Find(x => x.Name == name);
+    }
+  }
+}
