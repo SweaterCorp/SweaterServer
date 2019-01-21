65fb4502437107732e5597d9f60815e3019453a7
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-21 12:55:16 +0700

    [Refactor] Change ColorType to new ids

--- CommonLibraries/CommonLibraries/CommonTypes/PersonalColorType.cs
+++ CommonLibraries/CommonLibraries/CommonTypes/PersonalColorType.cs
@@ -6,10 +6,11 @@ namespace CommonLibraries.CommonTypes
   public class PersonalColorType : CustomEnum
   {
     public static PersonalColorType None { get; } = new PersonalColorType(0, "None");
-    public static PersonalColorType Winter { get; } = new PersonalColorType(1, "Winter");
+    public static PersonalColorType Autumn { get; } = new PersonalColorType(1, "Autumn");
     public static PersonalColorType Spring { get; } = new PersonalColorType(2, "Spring");
     public static PersonalColorType Summer { get; } = new PersonalColorType(3, "Summer");
-    public static PersonalColorType Autumn { get; } = new PersonalColorType(4, "Autumn");
+    public static PersonalColorType Winter { get; } = new PersonalColorType(4, "Winter");
+
 
     private PersonalColorType(int id, string name) : base(id, name)
     {
