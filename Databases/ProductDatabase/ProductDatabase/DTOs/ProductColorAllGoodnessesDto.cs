424400062d045fba0b4b7083a8d5880ff521d07d
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-23 04:01:35 +0700

    [Add] ProductColorAllGoodnessDto

---
+++ Databases/ProductDatabase/ProductDatabase/DTOs/ProductColorAllGoodnessesDto.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.CommonTypes;
+
+namespace ProductDatabase.DTOs
+{
+  public class ProductWithColorGoodnessDto
+  {
+    public int ProductId { get; set; }
+
+    public List<ProductColorGoodnessDto> ProductColorGoodnesses { get; set; }
+  }
+
+  public class ProductColorGoodnessDto
+  {
+    public int PersonalColorTypeId { get; set; }
+    public int ColorId { get; set; }
+    public float Goodness { get; set; }
+  }
+}
