148e615acf1c8562e14bdc808a27663b1da1f8f4
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-23 03:28:07 +0700

    [Add] ColorMatching

---
+++ Databases/ProductDatabase/ProductDatabase/DTOs/ColorMatching.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductDatabase.DTOs
+{
+  //public class ColorMatchingDto
+  //{
+  //  public int ColorIdÜgetösetö
+  //}
+
+  //public class ColorGroupDto
+  //{
+  //  public double 
+  //}
+}
