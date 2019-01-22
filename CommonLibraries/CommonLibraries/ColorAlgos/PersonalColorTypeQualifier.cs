c2702493ec5d2e26ea2b9190d55578933a867698
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-23 03:27:51 +0700

    [Add] PersonalColorTypeQualifier

---
+++ CommonLibraries/CommonLibraries/ColorAlgos/PersonalColorTypeQualifier.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CommonLibraries.CommonTypes;
+using CommonLibraries.Extensions;
+
+namespace CommonLibraries.ColorAlgos
+{
+  public class PersonalColorTypeQualifier
+  {
+    public PersonalColorCollection Collection { get; set; } = new PersonalColorCollection();
+
+    public PersonalColorType GetPersonalColorType(string eyeColor, string hairColor, string skinTone)
+    {
+      var ranking = new List<double>
+      {
+        BelongTo(Collection.Autumn),
+        BelongTo(Collection.Spring),
+        BelongTo(Collection.Summer),
+        BelongTo(Collection.Winter)
+      };
+
+      var max = ranking.Max();
+      if (ranking[0].EqualsStrict(max)) return PersonalColorType.Autumn;
+      if (ranking[1].EqualsStrict(max)) return PersonalColorType.Spring;
+      if (ranking[2].EqualsStrict(max)) return PersonalColorType.Summer;
+      return PersonalColorType.Winter;
+
+      double BelongTo(PersonalColor personalColor)
+      {
+        var result = 0.0;
+        if (personalColor.EyeColors.Contains(eyeColor)) result += 1.5;
+        if (personalColor.HairColors.Contains(hairColor)) result++;
+        if (personalColor.SkinTones.Contains(skinTone)) result++;
+        return result;
+      }
+    }
+  }
+}
