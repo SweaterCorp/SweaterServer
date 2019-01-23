f6937ba41a3d433fbca17555d7dc16514525efde
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-24 01:34:47 +0700

    [Add] ProductColorGoodnessEntity

---
+++ Databases/ProductDatabase/ProductDatabase/Entities/ProductColorGoodnessEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductDatabase.Entities
+{
+  [Table("ProductColorGoodness")]
+  public class ProductColorGoodnessEntity
+  {
+    [Key]
+    public int ProductColorGoodnessId { get; set; }
+
+    public int ProductId { get; set; }
+    public int PersonalColorTypeId { get; set; }
+    public float Goodness { get; set; }
+  }
+}
