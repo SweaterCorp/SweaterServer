73498f6ca31f73463e25b7e6ff35ca85b15b1c15
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-24 01:38:48 +0700

    [Add] New props to ProductsFilterVM

--- SweaterMain/SweaterMain/ViewModels/ProductsFilterViewModel.cs
+++ SweaterMain/SweaterMain/ViewModels/ProductsFilterViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SweaterMain.ViewModels
 {
-  public class ProductsFilterViewModel
+  public class ProductFilterViewModel
   {
+    [Required]
+    public int CategoryId { get; set; }
+    [Required]
+    public int PersonalColorTypeId { get; set; }
     public decimal MinimalPrice { get; set; } = decimal.MinValue;
     public decimal MaximalPrice { get; set; } = decimal.MaxValue;
     public List<int> BrandsIds { get; set; } = new List<int>();
-    public List<int> ColorsIds { get; set; } = new List<int>();
     public List<int> SizesIds { get; set; } = new List<int>();
     public PageParams PageParams { get; set; } = new PageParams();
   }
