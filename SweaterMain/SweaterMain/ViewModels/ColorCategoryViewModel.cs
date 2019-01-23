3fc5a4ae9b8970c7e4efad10e3358cece31cf157
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-24 01:38:14 +0700

    [Add] ColorCategoryVM

---
+++ SweaterMain/SweaterMain/ViewModels/ColorCategoryViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SweaterMain.ViewModels
+{
+  public class ColorCategoryViewModel
+  {
+    public ColorViewModel RedPink { get; set; }
+    public ColorViewModel OrangeYellow { get; set; }
+    public ColorViewModel Green { get; set; }
+    public ColorViewModel Blue { get; set; }
+    public ColorViewModel Purple { get; set; }
+    public ColorViewModel BrownBeige { get; set; }
+    public ColorViewModel GrayBlackWhite { get; set; }
+  }
+
+  public class ColorViewModel
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<string> Hexes { get; set; }
+
+  }
+
+}
