ecbea0920e8e1050b5d787a08d08f62d0e29fd26
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-24 01:35:18 +0700

    [Add] UserEntity

---
+++ Databases/ProductDatabase/ProductDatabase/Entities/UserEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProductDatabase.Entities
+{
+  [Table("User")]
+  public class UserEntity
+  {
+    [Key]
+    public int UserId { get; set; } // ReSharper disable InconsistentNaming
+
+    public string PhoneIMEI { get; set; }
+    public int PersonalColorTypeId { get; set; }
+  }
+}
