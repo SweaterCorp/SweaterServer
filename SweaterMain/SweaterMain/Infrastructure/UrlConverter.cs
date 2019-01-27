69211f228d84937187de546c6b4649be0ed2b710
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-28 02:04:23 +0700

    [Add] UrlConverter throug diff ImageSizes

---
+++ SweaterMain/SweaterMain/Infrastructure/UrlConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SweaterMain.Infrastructure
+{
+  public class UrlConverter
+  {
+    public static string Convert(string url, ConvertedType type)
+    {
+      var uri = new Uri(url);
+      var size = uri.LocalPath.Split("/")[1];
+
+      return url.Replace(size, type.ToString().ToLower());
+    }
+  }
+
+  public enum ConvertedType
+  {
+    Product = 0,
+    Img46X66 = 1,
+    Img135X194 = 2
+  }
+}
