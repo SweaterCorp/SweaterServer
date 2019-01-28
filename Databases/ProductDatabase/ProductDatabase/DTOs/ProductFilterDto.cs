133e9bf47c399ecba2d44b42c1473365bff07939
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-29 02:41:23 +0700

    [Add] ProductFilterDto

---
+++ Databases/ProductDatabase/ProductDatabase/DTOs/ProductFilterDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using CommonLibraries.CommonTypes;
+
+namespace ProductDatabase.DTOs
+{
+  public class ProductFilterDto
+  {
+    public int CategoryId { get; set; }
+    public decimal MinimalPrice { get; set; } = decimal.Zero;
+    public decimal MaximalPrice { get; set; } = decimal.MaxValue;
+    public List<int> BrandIds { get; set; } = new List<int>();
+    public PersonalColorType PersonalColorType { get; set; }
+    public List<int> SizeIds { get; set; } = new List<int>();
+  }
+}
