613a4c07902f48d21455edb478340110e2cbe413
Yuriy Aksenov <Yura290610@yandex.ru>
2019-01-31 20:17:54 +0700

    [Add] Monitoring Repo

---
+++ Databases/ProductDatabase/ProductDatabase/Repositories/MonitoringRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductDatabase.Repositories
+{
+  public class MonitoringRepository
+  {
+    private ProductContext Db { get; }
+
+    public MonitoringRepository(ProductContext db)
+    {
+      Db = db;
+    }
+
+    public async Task<bool> IncrementClickCategoryAsync(int categoryId)
+    {
+      var category = Db.CategoryEntities.First(x => x.CategoryId == categoryId);
+      category.ClicksCount++;
+      return await Db.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> IncrementClickProductAsync(int productId)
+    {
+      var product = Db.ProductEntities.First(x => x.ProductId == productId);
+      product.ClicksCount++;
+      return await Db.SaveChangesAsync() > 0;
+    }
+  }
+}
