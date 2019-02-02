e57f9ae6260f6d677b6baa98e29fd04c103e9889
Yuriy Aksenov <Yura290610@yandex.ru>
2019-02-02 16:32:57 +0700

    [Refacor] Combine two attrs into the one

--- SweaterMain/SweaterMain/ViewModels/CreateUserViewModel.cs
+++ SweaterMain/SweaterMain/ViewModels/CreateUserViewModel.cs
@@ -5,23 +5,19 @@ namespace SweaterMain.ViewModels
   public class CreateUserViewModel
   {
     [Required]
-    [MaxLength(17, ErrorMessage = "The IMEI of the phone required no more than 17 numbers.")]
-    [MinLength(15, ErrorMessage = "The IMEI of the phone required no less than 15 numbers.")]
+    [Range(15, 17, ErrorMessage = "The IMEI of the phone required no less than 15 and no more than 17 numbers.")]
     public string PhoneIMEI { get; set; }
 
     [Required]
-    [MaxLength(6, ErrorMessage = "The web color has no more than 6 hex numbers.")]
-    [MinLength(6, ErrorMessage = "The web color has no less than 6 hex numbers.")]
+    [Range(6,6, ErrorMessage = "The web color has 6 hex numbers.")]
     public string EyeColor { get; set; }
 
     [Required]
-    [MaxLength(6, ErrorMessage = "The web color has no more than 6 hex numbers.")]
-    [MinLength(6, ErrorMessage = "The web color has no less than 6 hex numbers.")]
+    [Range(6, 6, ErrorMessage = "The web color has 6 hex numbers.")]
     public string HairColor { get; set; }
 
     [Required]
-    [MaxLength(6, ErrorMessage = "The web color has no more than 6 hex numbers.")]
-    [MinLength(6, ErrorMessage = "The web color has no less than 6 hex numbers.")]
+    [Range(6, 6, ErrorMessage = "The web color has 6 hex numbers.")]
     public string SkinTone { get; set; }
   }
 }
