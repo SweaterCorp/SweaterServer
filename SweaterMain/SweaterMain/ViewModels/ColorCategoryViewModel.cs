e1ec8aa635ad1fea29a9bb5ed43471fca79c851a
Yuriy Aksenov <Yura290610@yandex.ru>
2019-03-05 21:13:26 +0300

    [Refactor]

--- SweaterMain/SweaterMain/ViewModels/ColorCategoryViewModel.cs
+++ SweaterMain/SweaterMain/ViewModels/ColorCategoryViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace SweaterMain.ViewModels
 {
@@ -21,7 +18,5 @@ namespace SweaterMain.ViewModels
     public int Id { get; set; }
     public string Name { get; set; }
     public List<string> Hexes { get; set; }
-
   }
-
 }
