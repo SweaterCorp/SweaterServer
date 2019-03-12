d039658c07c9acc99a9b8e22fbd071a3abc6330d
Yuriy Aksenov <Yura290610@yandex.ru>
2019-03-12 10:53:03 +0300

    [Change] To System enum the color group type

--- CommonLibraries/CommonLibraries/CommonTypes/ColorGroupType.cs
+++ CommonLibraries/CommonLibraries/CommonTypes/ColorGroupType.cs
@@ -1,31 +1,14 @@
-﻿using System.Collections.Generic;
-using CommonLibraries.Infrastructures;
-
-namespace CommonLibraries.CommonTypes
+﻿namespace CommonLibraries.CommonTypes
 {
-  public class ColorGroupType : CustomEnum
+  public enum ColorGroupType
   {
-    public static ColorGroupType None { get; } = new ColorGroupType(0, "None");
-    public static ColorGroupType RedPink { get; set; } = new ColorGroupType(1, "RedPink");
-    public static ColorGroupType OrangeYellow { get; set; } = new ColorGroupType(2, "OrangeYellow");
-    public static ColorGroupType Green { get; set; } = new ColorGroupType(3, "Green");
-    public static ColorGroupType Blue { get; set; } = new ColorGroupType(4, "Blue");
-    public static ColorGroupType Purple { get; set; } = new ColorGroupType(5, "Purple");
-    public static ColorGroupType BrownBeige { get; set; } = new ColorGroupType(6, "BrownBeige");
-    public static ColorGroupType GrayBlackWhite { get; set; } = new ColorGroupType(7, "GrayBlackWhite");
-
-    public ColorGroupType(int id, string name) : base(id, name)
-    {
-    }
-
-    public static List<ColorGroupType> AsList()
-    {
-      return new List<ColorGroupType> {None, RedPink, OrangeYellow, Green, Blue, Purple, BrownBeige, GrayBlackWhite};
-    }
-
-    public static explicit operator ColorGroupType(int id)
-    {
-      return AsList().Find(x => x.Id == id);
-    }
+    None = 0,
+    RedPink = 1,
+    OrangeYellow = 2,
+    Green = 3,
+    Blue = 4,
+    Purple = 5,
+    BrownBeige = 6,
+    GrayBlackWhite = 7
   }
 }
