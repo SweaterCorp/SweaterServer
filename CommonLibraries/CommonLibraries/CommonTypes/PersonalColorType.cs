8ef407a033e8a91471df5435ea53fe5e0d61915d
Yuriy Aksenov <Yura290610@yandex.ru>
2019-03-14 23:44:42 +0300

    [Change] PersonalTypes To enum

--- CommonLibraries/CommonLibraries/CommonTypes/PersonalColorType.cs
+++ CommonLibraries/CommonLibraries/CommonTypes/PersonalColorType.cs
@@ -1,34 +1,11 @@
-﻿using System.Collections.Generic;
-using CommonLibraries.Infrastructures;
-
-namespace CommonLibraries.CommonTypes
+﻿namespace CommonLibraries.CommonTypes
 {
-  public class PersonalColorType : CustomEnum
+  public enum PersonalColorType
   {
-    public static PersonalColorType None { get; } = new PersonalColorType(0, "None");
-    public static PersonalColorType Autumn { get; } = new PersonalColorType(1, "Autumn");
-    public static PersonalColorType Spring { get; } = new PersonalColorType(2, "Spring");
-    public static PersonalColorType Summer { get; } = new PersonalColorType(3, "Summer");
-    public static PersonalColorType Winter { get; } = new PersonalColorType(4, "Winter");
-
-
-    private PersonalColorType(int id, string name) : base(id, name)
-    {
-    }
-
-    public static List<PersonalColorType> AsList()
-    {
-      return new List<PersonalColorType> {None, Winter, Spring, Summer, Autumn};
-    }
-
-    public static explicit operator PersonalColorType(int id)
-    {
-      return AsList().Find(x => x.Id == id);
-    }
-
-    public static explicit operator PersonalColorType(string name)
-    {
-      return AsList().Find(x => x.Name == name);
-    }
+    None = 0,
+    Autumn = 1,
+    Spring = 2,
+    Summer = 3,
+    Winter = 4
   }
 }
