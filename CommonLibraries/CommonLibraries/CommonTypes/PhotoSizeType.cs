85a61b8a93ded54e8570b15cb7ac105455b2ab6d
Yuriy Aksenov <Yura290610@yandex.ru>
2019-03-21 15:32:26 +0300

    [Change] To system enum

--- CommonLibraries/CommonLibraries/CommonTypes/PhotoSizeType.cs
+++ CommonLibraries/CommonLibraries/CommonTypes/PhotoSizeType.cs
@@ -1,26 +1,9 @@
-﻿using System.Collections.Generic;
-using CommonLibraries.Infrastructures;
-
-namespace CommonLibraries.CommonTypes
+﻿namespace CommonLibraries.CommonTypes
 {
-  public class PhotoSizeType : CustomEnum
+  public enum PhotoSizeType
   {
-    public static PhotoSizeType None { get; } = new PhotoSizeType(0, "None");
-    public static PhotoSizeType Original { get; } = new PhotoSizeType(1, "Original");
-    public static PhotoSizeType Small { get; } = new PhotoSizeType(2, "Small");
-
-    public PhotoSizeType(int id, string name) : base(id, name)
-    {
-    }
-
-    public static List<PhotoSizeType> AsList()
-    {
-      return new List<PhotoSizeType> {None, Original, Small};
-    }
-
-    public static explicit operator PhotoSizeType(int id)
-    {
-      return AsList().Find(x => x.Id == id);
-    }
+    None = 0,
+    Original = 1,
+    Small = 2
   }
 }
