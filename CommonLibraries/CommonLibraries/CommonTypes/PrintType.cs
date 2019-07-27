8ff3255dc840cee4162d35a9069e2ba1729efcb9
Iurii Aksenov <iurii.aksenov@yandex.ru>
2019-07-27 12:25:54 +0300

    [Refactor] Custom enum to enum

--- CommonLibraries/CommonLibraries/CommonTypes/PrintType.cs
+++ CommonLibraries/CommonLibraries/CommonTypes/PrintType.cs
@@ -1,62 +1,18 @@
-﻿using System.Collections.Generic;
-using CommonLibraries.Infrastructures;
-
-namespace CommonLibraries.CommonTypes
+﻿namespace CommonLibraries.CommonTypes
 {
-  public class PrintType : CustomEnum
+  public enum PrintType
   {
-    public string RussianName { get; set; }
-
-    public static PrintType None { get; } = new PrintType(0, "None", "None");
-    public static PrintType Geometry { get; } = new PrintType(1, "Geometry;", "Геометрия");
-    public static PrintType PolkaDotted { get; } = new PrintType(2, "Polka-dotted", "Горох");
-    public static PrintType Other { get; } = new PrintType(3, "Other", "Другое");
-    public static PrintType Animals { get; } = new PrintType(4, "Animals", "Животные");
-    public static PrintType Camouflage { get; } = new PrintType(5, "Camouflage", "Камуфляж");
-    public static PrintType Checked { get; } = new PrintType(6, "Checked", "Клетка");
-    public static PrintType Spotted { get; } = new PrintType(7, "Spotted", "Леопардовый");
-    public static PrintType Plain { get; } = new PrintType(8, "Plain", "Однотонный");
-    public static PrintType Striped { get; } = new PrintType(9, "Striped", "Полоска");
-    public static PrintType Printed { get; } = new PrintType(10, "Printed", "Рисунки и надписи");
-    public static PrintType Floral { get; } = new PrintType(11, "Floral", "Цветочный");
-
-    private PrintType(int id, string name, string russianName = "") : base(id, name)
-    {
-      RussianName = russianName;
-    }
-
-    public static List<PrintType> AsList()
-    {
-      return new List<PrintType>
-      {
-        None,
-        Geometry,
-        PolkaDotted,
-        Other,
-        Animals,
-        Camouflage,
-        Checked,
-        Spotted,
-        Plain,
-        Striped,
-        Printed,
-        Floral
-      };
-    }
-
-    public static explicit operator PrintType(int id)
-    {
-      return AsList().Find(x => x.Id == id);
-    }
-
-    public static explicit operator PrintType(string name)
-    {
-      return AsList().Find(x => x.Name == name);
-    }
-
-    public static PrintType StringToPrintType(string name, bool isRussian = false)
-    {
-      return isRussian ? AsList().Find(x => x.RussianName.Equals(name, System.StringComparison.OrdinalIgnoreCase)) : (PrintType)name;
-    }
+    None = 0,
+    Geometry = 1,
+    PolkaDotted = 2,
+    Other = 3,
+    Animals = 4,
+    Camouflage = 5,
+    Checked = 6,
+    Spotted = 7,
+    Plain = 8,
+    Striped = 9,
+    Printed = 10,
+    Floral = 11
   }
 }
